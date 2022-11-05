a245ccfec8f82ef2540b622e8cd396ba193ae8ad
asbAmila <75632398+asbAmila@users.noreply.github.com>
2022-11-05 13:07:03 +0530

    Dashboard setup 3

--- routes/web.php
+++ routes/web.php
@@ -25,10 +25,18 @@ Route::get('/dashboard', function () {
 
 require __DIR__.'/auth.php';
 
+//Admin
 Route::middleware(['auth','role:admin'])->group(function (){
     Route::get('/admin/dashboard',[AdminController::class,'AdminDashboard']) ->name('admin.dashboard');
+
+    Route::get('/admin/logout',[AdminController::class,'AdminDestroy']) ->name('admin.logout');
 });
 
+
+
+
+
+//Vendor
 Route::middleware(['auth','role:vendor'])->group(function (){
     Route::get('/vendor/dashboard',[VendorController::class,'VendorDashboard']) ->name('vendor.dashboard');
 });
